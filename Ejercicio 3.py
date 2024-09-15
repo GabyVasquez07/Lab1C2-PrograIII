245f1a1cafcc7fc81a3ec1243735b923d0f85072
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-09-15 16:28:46 -0600

    Ajustes

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -15,22 +15,33 @@ class ventanap(QMainWindow):
         self.setWindowTitle('Ejercicio 3')
         layout= QVBoxLayout()
         central=QWidget(self)
-        self.nombre= QLabel("Ingresa tu nombre completo:")
-        self.entrada = QLineEdit()
-        self.DUI =QLabel("Ingresa tu numero de DUI:")
-        self.entrada2=QLineEdit()
-        boton = QPushButton("Haz click aqui")
+         # Etiqueta y campo de entrada para el nombre
+        self.nombre = QLabel("Ingresa tu nombre completo:")
+        self.entrada_N = QLineEdit()
+        # Etiqueta y campo de entrada para el número de DUI
+        self.DUI = QLabel("Ingresa tu número de DUI:")
+        self.entrada_D = QLineEdit()
+        boton = QPushButton("Subir información")
         boton.clicked.connect(self.mostrar_mensaje)  # Conectar el botón al método para mostrar la clave
         layout.addWidget(self.nombre)
-        layout.addWidget(self.entrada)
+        layout.addWidget(self.entrada_N)
         layout.addWidget(self.DUI)
-        layout.addWidget(self.entrada2)
+        layout.addWidget(self.entrada_D)
         layout.addWidget(boton)
         central.setLayout(layout)
         self.setCentralWidget(central)
 
     def mostrar_mensaje(self):
-        pass
+        #Obtener los valores del nombre y el dui
+        nombre = self.entrada_N.text()
+        dui = self.entrada_D.text()
+         # Verificar si ambos campos tienen datos
+        if not nombre or not dui:
+            QMessageBox.warning(self, "Advertencia", "Por favor, ingrese todos los datos.")
+        else:
+            # Mostrar los datos ingresados en un cuadro de diálogo
+            QMessageBox.information(self, "Datos Ingresados",
+                                    f"Nombre: {nombre}\nDUI: {dui}")
         
 app = QApplication(sys.argv)
 ventana = ventanap()
